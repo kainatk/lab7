0f73f3bc53433dc4107223f485403df574b2da35
kainatk <112647777+kainatk@users.noreply.github.com>
2023-03-30 15:22:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-}
+  var button = document.getElementById('entrybutton'); 
+  
+  
+  
+  
+  
 window.addEventListener('load', init);
