9ef36661600180c97c94f4f46a7ddaf58bdcfb84
kainatk <112647777+kainatk@users.noreply.github.com>
2023-03-30 15:26:55 -0500

    Update script.js
    edited with spacing

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function init(){
   function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Daniel Krieglstein: " + textbox.value);
+    alert("Kainat Khan: " + textbox.value);
   }
   button.addEventListener('click', showMeText);
   }
